7bc09abac677f94017a9994852d4e28c64546c69
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-17 17:45:53 -0800

    ./

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import 'react-native-gesture-handler';
 
 export default function App() {  
   return (
-
     <View style={styles.container}>
       <Text>Open up App.js to start working </Text>
       <StatusBar style="auto" />
@@ -14,7 +13,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
