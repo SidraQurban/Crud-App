c70abdf4dc5b60ed606bb2d6d9be469c01a7fa1f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-19 20:57:54 -0800

    Screen added

---
+++ app/navigation/Navigation.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from '../screen/Home';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+   <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+    </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default Navigation
