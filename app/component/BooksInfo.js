8ebae721c9fa6e480bbb4f0384dd9b9add361628
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-21 22:47:58 -0800

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from 'react'
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const BooksInfo = () => {
-  
+
 const [data,setData] = useState([]);    
 
 const getAPIData =async () =>{
+  
     const url="http://10.0.2.2:3000/books";   
     let result = await fetch(url);   
     result = await result.json();
