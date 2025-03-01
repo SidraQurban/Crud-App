a56e41276a22725093a8a78d8e745fe70b52b72d
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-28 23:18:03 -0800

    ./

--- app/screen/Home.js
+++ app/screen/Home.js
@@ -1,38 +1,33 @@
-import { View, Text, StyleSheet, StatusBar, ScrollView } from 'react-native';
-import React from 'react';
+import { View, Text,StyleSheet, StatusBar } from 'react-native'
+import React from 'react'
 import { responsiveFontSize, responsiveWidth } from 'react-native-responsive-dimensions';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BooksInfo from '../component/BooksInfo';
-import AddBooks from '../component/AddBooks';
-
 const Home = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <>
+    <SafeAreaView>
       <View style={styles.container}>
-        <Text style={styles.mainText}> MyBookList </Text>
+        <Text style={styles.mainText}>MyBookList </Text>
       </View>
-
-      {/* Added ScrollView to prevent overflow issues */}
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <BooksInfo />
-        <AddBooks />
-      </ScrollView>
-
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
     </SafeAreaView>
+     <BooksInfo />
+     
+     <StatusBar barStyle="light-content" backgroundColor="#000" />
+    </>
   );
 }
 
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {
-    padding: responsiveWidth(2),
-    backgroundColor: '#edede9',
-  },
-  mainText: {
-    fontSize: responsiveFontSize(2.5),
+   container: {
+    padding:responsiveWidth(1),
+    backgroundColor: '#edede9',   
+   },
+   mainText: {
+    fontSize: responsiveFontSize(2),
     fontWeight: 'bold',
     textAlign: 'center',
-  },
+   }
 });
