a19c6fb260bce2c96bb0dd376bc1975ed125ae0f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-04 22:37:32 -0800

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -13,10 +13,11 @@ const[authorName,setAuthorName] = useState('');
 // const[uploadImageError,setUploadImageError] = useState('');
 const[bookNameError,setBookNameError] = useState('');
 const[authorNameError,setAuthorNameError] = useState('');
+
 useEffect(()=>{ 
   getAPIData(); 
   },[])    
-  
+
 const getAPIData =async () =>{
 const url="http://10.0.2.2:3000/books";
     let result = await fetch(url);
