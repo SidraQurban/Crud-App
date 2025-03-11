a2c27bf95623e852e775fdde29ef69f0e32f25ad
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-10 23:59:14 -0700

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -254,7 +254,7 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     backgroundColor: "red",
     borderRadius: responsiveHeight(5),
-    padding: responsiveHeight(1),    
+    dding: responsiveHeight(1),    
 
 
   }
