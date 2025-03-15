a4a7bb5a13eeec386bd1adcf3160a4c321f3f322
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-14 22:22:06 -0700

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -20,6 +20,7 @@ const url="http://10.0.2.2:3000/books";
     let result = await fetch(url);
     result = await result.json();
     console.log(result); 
+    
     setData(result);    
 }
 const saveData= async()=>{
