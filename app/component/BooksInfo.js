5f3de07630c53db1974d025db16dfe636bd73bd4
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-18 23:57:16 -0700

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -234,10 +234,11 @@ const styles = StyleSheet.create({
     padding: responsiveHeight(1.5),
     borderRadius: responsiveHeight(1),
     alignItems: "center", 
+    
   },
   cancelbtn: {
     alignSelf: "flex-end",
-    
+
     backgroundColor: "red",
     borderRadius: responsiveHeight(5),
     padding: responsiveHeight(1),    
