c6b21656155f54f3de9ecd2bdfea6f685c369208
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-21 14:29:02 -0700

    ./

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -7,7 +7,6 @@ const AppNavigator = () => {
   return (
    <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown: false}}>
-
    <Stack.Screen name='Home' component={Home} />
     </Stack.Navigator>
    </NavigationContainer>  
