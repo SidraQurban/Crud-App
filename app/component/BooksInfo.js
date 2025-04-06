3c1e7dfcdeee093efa7499d62bb014d786f09db2
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-04-05 23:37:25 -0700

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -14,7 +14,6 @@ const[authorNameError,setAuthorNameError] = useState('');
 useEffect(()=>{ 
   getAPIData(); 
   },[])
-
 const getAPIData =async () =>{
 const url="http://10.0.2.2:3000/books"; 
     let result = await fetch(url);
