d886107bf27c4fe0b132617aeb1ac4b99fdd108e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-04-15 22:49:06 -0700

    ./

--- app/component/BooksInfo.js
+++ app/component/BooksInfo.js
@@ -240,6 +240,6 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     borderRadius: responsiveHeight(5),
     padding: responsiveHeight(1),  
-    marginTop: responsiveHeight(-3),  
+    marginTop: responsiveHeight(-3),
   }
 });
